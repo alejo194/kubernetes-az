54f581979eed8791cc53acfb2831a3a285a4df9d
alejo194 <37093748+alejo194@users.noreply.github.com>
2021-05-27 15:56:59 +0800

    Create shell用例6.sh

---
+++ linux/shell/shell用例6.sh
@@ -0,0 +1,12 @@
+cat chkweb.sh
+#!/bin/bash
+TARGETS=$(cat /root/mywebs.txt)
+PROBE=".feeler.html"
+for i in $TARGETS
+do
+    echo "##目标站点 $i"
+    echo -n "      端口状态："
+    nmap -sS $i -p 80 |awk '/80\/tcp/{print $2}'
+    echo -n "     业务系统状态："
+    wget -q http://$i/$PROBE  && echo ok || echo not ok
+done
